8c66caed9c03b00a895ea2b8c14a06546f1552ad
Albatroz Jeremias <ajeremias@disroot.org>
2021-09-02 12:45:48 +0100

    add first CRUD for a user

---
+++ src/models/user.model.ts
@@ -0,0 +1,45 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+  })
+  displayName?: string;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  emailConfirmed?: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
